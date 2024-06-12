c12070566d3fd4378bf96ba96b477a4d8fc5c6b6
fred-maina <fchege04@gmail.com>
2024-06-12 21:08:37 +0300

    Update test Urls to match new urls

--- api/tests.py
+++ api/tests.py
@@ -15,21 +15,21 @@ class QuizAPITestCase(APITestCase):
 
     def test_get_quizzes(self):
         # Test the quizzes view
-        response = self.client.get('/quizzes/')
+        response = self.client.get('/api/quizzes/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('quizzes', response.data)
         self.assertEqual(len(response.data['quizzes']), 1)
 
     def test_get_quiz_detail(self):
         # Test the quiz_detail view
-        response = self.client.get(f'/quizzes/{self.quiz.code}/')
+        response = self.client.get(f'/api/quizzes/{self.quiz.code}/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['title'], self.quiz.title)
         self.assertEqual(response.data['description'], self.quiz.description)
 
     def test_get_questions_by_quiz(self):
         # Test the questions_by_quiz view
-        response = self.client.get(f'/quizzes/{self.quiz.code}/questions/')
+        response = self.client.get(f'/api/quizzes/{self.quiz.code}/questions/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['quiz_code'], self.quiz.code)
         self.assertEqual(len(response.data['questions']), 1)
@@ -49,7 +49,7 @@ class QuizAPITestCase(APITestCase):
                 }
             ]
         }
-        response = self.client.post('/create/', data, format='json')
+        response = self.client.post('/api/create/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue(Quiz.objects.filter(title="New Quiz").exists())
         self.assertTrue(Question.objects.filter(question_text="New Question").exists())
