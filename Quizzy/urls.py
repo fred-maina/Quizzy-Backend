26ae78aa33d0487ee0515093c2a04affdf42ed94
fred-maina <fchege04@gmail.com>
2024-06-17 22:29:57 +0300

    Add Url configurations for displaying Quiz data

--- Quizzy/urls.py
+++ Quizzy/urls.py
@@ -4,5 +4,6 @@ urlpatterns=[
     path("",views.index,name="index"),
     path("login/",views.login,name="login_page"),
     path("dashboard/",views.dashboard,name="dashboard"),
-    path("add/",views.add,name="add_quiz")
+    path("add/",views.add,name="add_quiz"),
+    path('quiz/<str:quiz_code>/', views.display_quiz, name='display')
 ]
