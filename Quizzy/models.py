02542e13bb56e254db644841d83d6093311e1914
fred-maina <fchege04@gmail.com>
2024-06-29 00:25:02 +0300

    Ensure One user can Only have one score in a quiz

--- Quizzy/models.py
+++ Quizzy/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from api.models import Quiz
 
 class Leaderboard(models.Model):
-    quiz=models.ForeignKey(Quiz,on_delete=models.CASCADE)
-    score=models.IntegerField()
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['quiz', 'user'], name='unique_quiz_user')
+        ]
