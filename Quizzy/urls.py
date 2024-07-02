7b3aa7d92bf4124b2319faa418c51b4209b725d9
fred-maina <fchege04@gmail.com>
2024-07-02 21:13:17 +0300

    Refactor urls.py

--- Quizzy/urls.py
+++ Quizzy/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-urlpatterns=[
-    path("",views.index,name="index"),
-    path("login/",views.login,name="login_page"),
-    path("dashboard/",views.dashboard,name="dashboard"),
-    path("add/",views.add,name="add_quiz"),
-    path('quiz/<str:quiz_code>/', views.quiz,name='display')
-    
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login, name="login_page"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("add/", views.add, name="add_quiz"),
+    path('quiz/<str:quiz_code>/', views.quiz, name='display')
+
 ]
