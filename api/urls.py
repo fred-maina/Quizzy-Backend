e7f0a4566592fc499ab08f286d612f0ba41eeac9
fred-maina <fchege04@gmail.com>
2024-07-13 20:44:20 +0300

    Add url for patch requests

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 # urls.py
 from django.urls import path
-from .views import quizzes, quiz_detail, questions_by_quiz, create, delete_quiz
+from .views import quizzes, quiz_detail, questions_by_quiz, create, delete_quiz , update_question
 
 urlpatterns = [
     path('quizzes/', quizzes, name='quizzes'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('quizzes/<str:quiz_code>/', quiz_detail, name='quiz_detail'),  # Update URL pattern to use 'quiz_code'
     path('quizzes/<str:quiz_code>/questions/', questions_by_quiz, name='questions_by_quiz'),
     path('quizzes/<str:quiz_code>/delete/', delete_quiz, name='delete-quiz'),
+    path('quizzes/<str:quiz_code>/patch/', update_question, name="update_question"),
 
 ]
