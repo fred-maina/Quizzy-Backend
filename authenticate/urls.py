f6719a9df0dd7f0a9903010b01b9cdd2ff15d2e8
fred-maina <fchege04@gmail.com>
2024-07-16 18:19:12 +0300

    Add register path

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import register, login
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                             TokenRefreshView,)
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+
 
 urlpatterns = [
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', register, name='register')
 
 ]
-
