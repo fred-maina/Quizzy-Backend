8437afaafeb39b8985721f1b069e9166588401a6
fred-maina <fchege04@gmail.com>
2024-07-24 23:04:32 +0300

    Add Get user url

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 # urls.py
 from django.urls import path
-from .views import quizzes, quiz_detail, questions_by_quiz, create, delete_quiz , update_question
+from .views import quizzes, quiz_detail, questions_by_quiz, create, delete_quiz , update_question, get_user
 
 urlpatterns = [
     path('quizzes/', quizzes, name='quizzes'),
     path('create/',create,name='create'),
+    path('user',get_user,name="get_user"),
     path('quizzes/<str:quiz_code>/', quiz_detail, name='quiz_detail'),  # Update URL pattern to use 'quiz_code'
     path('quizzes/<str:quiz_code>/questions/', questions_by_quiz, name='questions_by_quiz'),
     path('quizzes/<str:quiz_code>/delete/', delete_quiz, name='delete-quiz'),
