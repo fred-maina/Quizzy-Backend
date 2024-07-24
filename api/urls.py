d37a6bf450d033c5b5018d7b873a92674de25764
fred-maina <fchege04@gmail.com>
2024-07-24 23:42:58 +0300

    change view name

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
 # urls.py
 from django.urls import path
-from .views import quizzes, quiz_detail, questions_by_quiz, create, delete_quiz , update_question, get_user
+from .views import quizzes, quiz_detail, questions_by_quiz, create, delete_quiz , update_question, get_user_info
 
 urlpatterns = [
     path('quizzes/', quizzes, name='quizzes'),
     path('create/',create,name='create'),
-    path('user',get_user,name="get_user"),
+    path('user',get_user_info,name="get_user"),
     path('quizzes/<str:quiz_code>/', quiz_detail, name='quiz_detail'),  # Update URL pattern to use 'quiz_code'
     path('quizzes/<str:quiz_code>/questions/', questions_by_quiz, name='questions_by_quiz'),
     path('quizzes/<str:quiz_code>/delete/', delete_quiz, name='delete-quiz'),
