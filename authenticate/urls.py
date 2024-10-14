e49a923ecbf0ff5a386277c2f94b0643259e8109
fred-maina <fchege04@gmail.com>
2024-10-14 18:52:40 +0300

    change URL to point to new views

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from .views import register, login
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .otp_verification import send_otp, verify_otp  # Import OTP views
 
 urlpatterns = [
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # JWT Token routes
+    path('auth/token/', login, name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', register, name='register')
 
+    # User Registration route
+    path('register/', register, name='register'),
+
+    # OTP routes
+    path('send-otp/', send_otp, name='send_otp'),  # Send OTP route
+    path('verify-otp/', verify_otp, name='verify_otp'),  # Verify OTP route
 ]
